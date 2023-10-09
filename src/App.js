d1406bc9dbc827631408eeeb19832a59a9a23681
sunil_swizy <sunil@iamneo.ai>
2023-10-10 00:29:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [data, setData] = useState([]);
+  const [msg, setMsg] = useState('');
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io('ws://backend-pobvgj2xoq-uc.a.run.app');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  const changeState = (msg) => {
+    setData((prevData) => [...prevData, msg]);
+  }
+
+  useEffect(() => {
+    if (socket) {
+      const handleHello = (arg) => {
+        changeState(arg);
+      }
+
+      socket.on("message", handleHello);
+
+      return () => {
+        socket.off("message", handleHello);
+      };
+    }
+  }, [socket])
+
+  const sendMsg = () => {
+
+    if(!name) return;
+
+    socket.emit("message", `${name}: ${msg}`);
+    setMsg('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input className='input' type="text" placeholder='enter your name' value={name} onChange={(e) => setName(e.target.value)}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input className='input' type="text" placeholder='type something..' value={msg} onChange={(e) => setMsg(e.target.value)}/>
+          <button className='btn' onClick={sendMsg}>send</button>
         </p>
-        <a
+        <div
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          {
+            data.map((el, idx) => (
+              <div key={idx}>{ el }</div>
+            ))
+          }
+        </div>
       </header>
     </div>
   );
